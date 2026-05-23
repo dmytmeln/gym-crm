0a4f11e73653c22a5dd0d72f502e8ed87c80122d
Dmytro Melnyk <dimamel28@gmail.com>
2026-05-24 00:21:16 +0300

    GIA-137 Add Swagger documentation

--- src/main/java/com/gym/crm/controller/AuthRestController.java
+++ src/main/java/com/gym/crm/controller/AuthRestController.java
@@ -1,8 +1,14 @@
 package com.gym.crm.controller;
 
+import com.gia.openapi.model.ErrorResponse;
 import com.gia.openapi.model.LoginChangeRequest;
 import com.gia.openapi.model.LoginRequest;
 import com.gym.crm.facade.GymFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +21,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${app.api.base-path}/auth")
 @RequiredArgsConstructor
+@Tag(name = "User Profile", description = "Login and account management")
 public class AuthRestController {
 
     private final GymFacade facade;
 
     @PutMapping("/password")
+    @Operation(
+            summary = "Change login password",
+            description = "Changes the password for a given user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Password changed successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input data",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized access",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Invalid user credentials",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<Void> changePassword(@Valid @RequestBody LoginChangeRequest loginChangeRequest) {
         facade.changePassword(loginChangeRequest.getUsername(), loginChangeRequest);
 
@@ -27,6 +76,40 @@ public class AuthRestController {
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Login with username and password",
+            description = "Authenticates user by username and password",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful login"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input data",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Invalid user credentials",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest loginRequest) {
         facade.login(loginRequest);
 
