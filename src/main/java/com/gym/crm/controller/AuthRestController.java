c9e3112e8061370f0d569f344ecb10291fc9e36f
Dmytro Melnyk <dimamel28@gmail.com>
2026-06-05 22:23:17 +0300

    GIA-214 Implement logout - update API specification

--- src/main/java/com/gym/crm/controller/AuthRestController.java
+++ src/main/java/com/gym/crm/controller/AuthRestController.java
@@ -77,6 +77,8 @@ public class AuthRestController {
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "401", description = "Unauthorized access",
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "Internal server error",
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))})
     public ResponseEntity<Void> logout(@RequestHeader(AUTHORIZATION) String authHeader) {
